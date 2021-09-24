bc3ed7a70243111ce4c3595767d1ffbfee5c216f
TortasMcFly <hectorluevanov@gmail.com>
2021-09-24 15:41:15 -0500

    setting data source and repos

---
+++ app/src/main/java/com/example/noteapp/feature_note/data/repository/NoteRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.example.noteapp.feature_note.data.repository
+
+import com.example.noteapp.feature_note.domain.model.Note
+import com.example.noteapp.feature_note.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepositoryImpl: NoteRepository{
+    override fun getNotes(): Flow<List<Note>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertNote(note: Note) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        TODO("Not yet implemented")
+    }
+}
