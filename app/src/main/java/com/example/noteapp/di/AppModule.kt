2834bc8c4d584ad3e8c5dcd876d2cc4bd95e01cb
TortasMcFly <hectorluevanov@gmail.com>
2021-10-01 10:04:22 -0500

    GetNoteUseCase added

--- app/src/main/java/com/example/noteapp/di/AppModule.kt
+++ app/src/main/java/com/example/noteapp/di/AppModule.kt
@@ -5,10 +5,7 @@ import androidx.room.Room
 import com.example.noteapp.feature_note.data.data_source.NoteDatabase
 import com.example.noteapp.feature_note.data.repository.NoteRepositoryImpl
 import com.example.noteapp.feature_note.domain.repository.NoteRepository
-import com.example.noteapp.feature_note.domain.use_case.AddNoteUseCase
-import com.example.noteapp.feature_note.domain.use_case.DeleteNoteUseCase
-import com.example.noteapp.feature_note.domain.use_case.GetNotesUseCase
-import com.example.noteapp.feature_note.domain.use_case.NoteUseCases
+import com.example.noteapp.feature_note.domain.use_case.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -41,6 +38,7 @@ object AppModule {
         return NoteUseCases(
             addNoteUseCase = AddNoteUseCase(repository),
             getNotesUseCase = GetNotesUseCase(repository),
+            getNoteUseCase = GetNoteUseCase(repository),
             deleteNoteUseCase = DeleteNoteUseCase(repository)
         )
     }
--- app/src/main/java/com/example/noteapp/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/example/noteapp/feature_note/domain/use_case/NoteUseCases.kt
@@ -3,5 +3,6 @@ package com.example.noteapp.feature_note.domain.use_case
 data class NoteUseCases(
     val addNoteUseCase: AddNoteUseCase,
     val getNotesUseCase: GetNotesUseCase,
+    val getNoteUseCase: GetNoteUseCase,
     val deleteNoteUseCase: DeleteNoteUseCase
 )
